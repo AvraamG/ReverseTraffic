2ad810f642497cb03b27974b45e9cadb8d3bca77
VladimirLi <vladde1991@gmail.com>
2020-01-18 17:52:41 +0000

    InputMannager + player

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private KeyCode menuKey;
+    [SerializeField] private GameManager gm;
+    [SerializeField] private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(menuKey))
+        {
+            // Spwan the menu
+        }
+        float horizontalMotion = Input.GetAxis("Horizontal");
+        float verticalMotion = Input.GetAxis("Vertical");
+
+        Vector3 motionVector = new Vector3(horizontalMotion, 0f, verticalMotion);
+        motionVector.Normalize();
+        player.Move(motionVector);
+
+
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float motionSpeed = 3f;
+    [SerializeField] private bool teleporting;
+    private bool isDead;
+    [SerializeField] private int health, maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+        health = maxHealth;
+    }
+    
+
+    public void Move(Vector3 motionVector)
+    {
+        transform.position += motionVector * motionSpeed * Time.deltaTime;
+    }
+
+    public void TakeDamage( int damage) {
+        if (health > 0)
+        {
+            health -= damage;
+        }
+        if (health <= 0) 
+        {
+            health = 0;
+            isDead = true;
+        }
+    }
+
+    public void Heal(int healing_points ) {
+        if (health < maxHealth)
+            health += healing_points;
+        if (health > maxHealth)
+            health = maxHealth;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Teleport"))
+        {
+            if (!teleporting)
+            {
+                Teleport(other.gameObject);
+            }
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Teleport"))
+        {
+            teleporting = false;
+        }
+    }
+
+    public void Teleport(GameObject pad)
+    {
+        int index = pad.transform.GetSiblingIndex();
+        GameObject siblingPad;
+        if (index == 0)
+        {
+            siblingPad = pad.transform.parent.GetChild(index + 1).gameObject;
+        }
+        else
+        {
+            siblingPad = pad.transform.parent.GetChild(index - 1).gameObject;
+        }
+        transform.position = siblingPad.transform.position + siblingPad.transform.TransformVector(0, 0, 1);
+        teleporting = true;
+    }
+}
