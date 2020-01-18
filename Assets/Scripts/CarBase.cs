0e9b77c59d855fb75e8d1b62f17720735fed3f70
Abraham Georgiadis <abraham@manomotion.com>
2020-01-18 18:35:39 +0100

    Meta and Scripts

---
+++ Assets/Scripts/CarBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBase : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shouldMove = true;
+    }
+    public bool shouldMove = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldMove)
+        {
+            this.transform.Translate(Vector3.forward * Time.deltaTime);
+
+        }
+
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            shouldMove = false;
+            CarManager.Instance.DestroyCar(this);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/CarManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarManager : MonoBehaviour
+{
+    private static CarManager _instance;
+    public static CarManager Instance { get { return _instance; } }
+
+
+    [SerializeField]
+    GameObject[] spawnPoints;
+
+    //There can only be 10 cars at a given time on the road
+    int maxCarsNumber = 10;
+    public int currentCarsOnRoad = 0;
+
+
+    [SerializeField]
+    GameObject genericCar;
+    [SerializeField]
+    GameObject policeCar;
+    [SerializeField]
+    GameObject taxiCar;
+
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+
+    void Start()
+    {
+        if (spawnPoints.Length <= 0)
+        {
+            spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        }
+
+        //Spawn a couple of cars.
+
+
+
+
+        for (int i = 0; i < maxCarsNumber; i++)
+        {
+
+        }
+    }
+
+    //Call this method when the GameManagerSays StartGame
+    public void InitializeCarLane()
+    {
+        //IF Game Status is PLAYING
+        //IF CarsOnRoad < MaxCARS
+        //SPAWN CARs
+    }
+
+    public void SpawnRandomCar()
+    {
+        int randomSpawnPoint = Random.RandomRange(0, spawnPoints.Length);
+        //   int randomSpawnPoint = 0;
+
+        int randomCarType = Random.Range(0, 3);
+
+        switch (randomCarType)
+        {
+            case 0:
+                Instantiate(genericCar, spawnPoints[randomSpawnPoint].transform.position, spawnPoints[randomSpawnPoint].transform.rotation);
+                break;
+            case 1:
+                Instantiate(taxiCar, spawnPoints[randomSpawnPoint].transform.position, spawnPoints[randomSpawnPoint].transform.rotation);
+                break;
+            case 2:
+                Instantiate(policeCar, spawnPoints[randomSpawnPoint].transform.position, spawnPoints[randomSpawnPoint].transform.rotation);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void DestroyCar(CarBase car)
+    {
+
+        SpawnRandomCar();
+
+        Destroy(car.gameObject);
+    }
+
+    public void SpawnCar()
+    {
+        //Check of the difficulty
+
+        //Find the first unCappedSpawnPoint
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnRandomCar();
+        }
+    }
+}
